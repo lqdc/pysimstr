c4b8420b7239746ce3c43f5a5eb805cc6c5b6cfb
rsinayev <roman.sinayev@gmail.com>
2015-05-21 01:19:00 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='pysimstr',
+    packages=['pysimstr'],
+    version='0.1',
+    description='Fast(ish) string similarity for one vs many comparisons',
+    author='Roman Sinayev',
+    author_email='roman.sinayev@gmail.com',
+    url='https://github.com/lqdc/pysimstr',
+    download_url='https://github.com/lqdc/pysimstr/tarball/0.1',
+    keywords=['jaro-winkler', 'jaro', 'string similarity', 'levenshtein'],
+    classifiers=[],
+)
